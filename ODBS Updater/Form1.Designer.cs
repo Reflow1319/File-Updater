64a21686a543019adad5c9a1a06c39e77a16c101
Reflow1319 <admin@xbitestudio.com>
2015-12-12 17:58:44 -0600

    GUI
    UPDATER/LAUNCHER APP

---
+++ ODBS Updater/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace ODBS_Updater
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.up = new System.Windows.Forms.Button();
+            this.progress = new System.Windows.Forms.ProgressBar();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.web = new System.Windows.Forms.WebBrowser();
+            this.stat = new System.Windows.Forms.Label();
+            this.pro = new System.Windows.Forms.Label();
+            this.play = new System.Windows.Forms.Button();
+            this.current = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.onstat = new System.Windows.Forms.WebBrowser();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // up
+            // 
+            this.up.Location = new System.Drawing.Point(1028, 850);
+            this.up.Name = "up";
+            this.up.Size = new System.Drawing.Size(435, 47);
+            this.up.TabIndex = 0;
+            this.up.Text = "UPDATE";
+            this.up.UseVisualStyleBackColor = true;
+            this.up.Click += new System.EventHandler(this.up_Click);
+            // 
+            // progress
+            // 
+            this.progress.Location = new System.Drawing.Point(12, 849);
+            this.progress.Name = "progress";
+            this.progress.Size = new System.Drawing.Size(992, 47);
+            this.progress.TabIndex = 1;
+            this.progress.Click += new System.EventHandler(this.progress_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.web);
+            this.panel1.Location = new System.Drawing.Point(4, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1813, 754);
+            this.panel1.TabIndex = 2;
+            // 
+            // web
+            // 
+            this.web.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.web.Location = new System.Drawing.Point(0, 0);
+            this.web.MinimumSize = new System.Drawing.Size(20, 20);
+            this.web.Name = "web";
+            this.web.Size = new System.Drawing.Size(1813, 754);
+            this.web.TabIndex = 0;
+            this.web.Url = new System.Uri("", System.UriKind.Relative);
+            this.web.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // stat
+            // 
+            this.stat.AutoSize = true;
+            this.stat.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.06283F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stat.Location = new System.Drawing.Point(12, 791);
+            this.stat.Name = "stat";
+            this.stat.Size = new System.Drawing.Size(206, 38);
+            this.stat.TabIndex = 3;
+            this.stat.Text = "Status: None";
+            // 
+            // pro
+            // 
+            this.pro.AutoSize = true;
+            this.pro.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.06283F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pro.Location = new System.Drawing.Point(463, 792);
+            this.pro.Name = "pro";
+            this.pro.Size = new System.Drawing.Size(212, 38);
+            this.pro.TabIndex = 4;
+            this.pro.Text = "Progress: 0/0";
+            this.pro.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // play
+            // 
+            this.play.Location = new System.Drawing.Point(1028, 783);
+            this.play.Name = "play";
+            this.play.Size = new System.Drawing.Size(435, 47);
+            this.play.TabIndex = 5;
+            this.play.Text = "WEBSITE";
+            this.play.UseVisualStyleBackColor = true;
+            this.play.Click += new System.EventHandler(this.play_Click);
+            // 
+            // current
+            // 
+            this.current.AutoSize = true;
+            this.current.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.06283F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.current.Location = new System.Drawing.Point(722, 792);
+            this.current.Name = "current";
+            this.current.Size = new System.Drawing.Size(282, 38);
+            this.current.TabIndex = 6;
+            this.current.Text = "Current Version: 0";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.onstat);
+            this.panel2.Location = new System.Drawing.Point(1487, 771);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(321, 125);
+            this.panel2.TabIndex = 7;
+            // 
+            // onstat
+            // 
+            this.onstat.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.onstat.Location = new System.Drawing.Point(0, 0);
+            this.onstat.MinimumSize = new System.Drawing.Size(20, 20);
+            this.onstat.Name = "onstat";
+            this.onstat.ScrollBarsEnabled = false;
+            this.onstat.Size = new System.Drawing.Size(321, 125);
+            this.onstat.TabIndex = 0;
+            this.onstat.Url = new System.Uri("", System.UriKind.Relative);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1829, 909);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.current);
+            this.Controls.Add(this.play);
+            this.Controls.Add(this.pro);
+            this.Controls.Add(this.stat);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.progress);
+            this.Controls.Add(this.up);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button up;
+        private System.Windows.Forms.ProgressBar progress;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.WebBrowser web;
+        private System.Windows.Forms.Label stat;
+        private System.Windows.Forms.Label pro;
+        private System.Windows.Forms.Button play;
+        private System.Windows.Forms.Label current;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.WebBrowser onstat;
+
+    }
+}
+
---
+++ ODBS Updater/Form1.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Timers;
+using System.Threading;
+using MySql.Data.MySqlClient;
+
+namespace ODBS_Updater
+{
+    public partial class Form1 : Form
+    {
+        string startupPath = AppDomain.CurrentDomain.BaseDirectory;
+        string settings;
+        string[] setarry;
+        //private string conn;
+       // private MySqlConnection connect;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Form1 f1 = new Form1();
+            settings = startupPath + "settings.txt";
+            setarry = File.ReadAllLines("settings.txt");
+            this.Text = setarry[7];
+            string revisionFile = startupPath + "Revision.txt";
+            current.Text = "Current Version: " + File.ReadAllText(revisionFile);
+            this.web.Navigate(setarry[5]);
+            this.onstat.Navigate(setarry[9]);
+
+            string hostFile = startupPath + "Host.txt";
+
+            string baseURL = File.ReadAllText(hostFile);
+            // Download and convert the file list into a byte stream.
+            var fileList = new DataBuffer(DownloadFile(baseURL + "FileList.dat"));
+
+            // Get the current product revision as determined by the FileIndexer
+            // by the number of patch folders, and store it for later use.
+            int serverRevision = fileList.ReadInt();
+
+            if (serverRevision == Int32.Parse(File.ReadAllText(revisionFile)) )
+            {
+                up.Text = "PLAY";
+            }else {
+                up.Text = "UPDATE";
+            }
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void up_Click(object sender, EventArgs e)
+        {
+            if (up.Text == "UPDATE")
+            {
+                // Necessary paths and URLs.
+                string startupPath = AppDomain.CurrentDomain.BaseDirectory;
+                string revisionFile = startupPath + "Revision.txt";
+                string hostFile = startupPath + "Host.txt";
+                stat.Text = "Status: UPDATING";
+
+                // Check the host file. If it does not exist, or the
+                // specified url is blank, let the user know and close the updater.
+                string baseURL = "";
+
+                if (File.Exists(hostFile))
+                {
+                    baseURL = File.ReadAllText(hostFile);
+                }
+                else
+                {
+                    File.WriteAllText(hostFile, "");
+                }
+                if (baseURL == "")
+                {
+                    CloseProgram("Please write the host you are trying to receive an update from in your Host.txt file.");
+                }
+
+                // Check the revision file. If it does not exist,
+                // leave the revision at 0 to signify a lack of files.
+                int clientRevision = 0;
+                if (File.Exists(revisionFile))
+                {
+                    clientRevision = Int32.Parse(File.ReadAllText(revisionFile));
+                }
+
+
+                // Download and convert the folder list into a byte stream and create 
+                // the necessary folders.
+                var folderList = new DataBuffer(DownloadFile(baseURL + "FolderList.dat"));
+                int folderCount = folderList.ReadInt();
+                for (int i = 0; i < folderCount; i++)
+                {
+                    Directory.CreateDirectory(startupPath + folderList.ReadString());
+                }
+
+                // Download and convert the file list into a byte stream.
+                var fileList = new DataBuffer(DownloadFile(baseURL + "FileList.dat"));
+
+                // Get the current product revision as determined by the FileIndexer
+                // by the number of patch folders, and store it for later use.
+                int serverRevision = fileList.ReadInt();
+
+                // Create a webclient for downloading files and instantly saving them
+                // onto the disk.
+                var client = new WebClient();
+
+                // Loop through the file list and retrieve information about the required files.
+                int fileCount = fileList.ReadInt();
+                for (int i = 0; i < fileCount; i++)
+                {
+                    string clientpath = fileList.ReadString();
+                    string serverpath = fileList.ReadString();
+                    int revision = fileList.ReadInt();
+                    int j = 1 + i;
+                    pro.Text = "Progress: " + j + "/" + fileCount.ToString();
+
+                    // If the file already exists and the revision it was introduced in
+                    // is older than our current version, then continue to the next
+                    // file in the file list.
+                    if (File.Exists(startupPath + clientpath) && revision <= clientRevision)
+                    {
+                        continue;
+                    }
+
+                    // If the file doesn't exist, or if the file exists but was 
+                    // introduced in a newer version than our current version, then download the file.
+                    Console.WriteLine("Downloading " + clientpath);
+
+                    progress.Increment(10);
+
+                    client.DownloadFile(baseURL + serverpath, startupPath + clientpath);
+                    // Task.Delay(300);
+                }
+
+
+                // Store the current revision we installed, and terminate the program
+                // with a message of success.
+                File.WriteAllText(revisionFile, serverRevision.ToString());
+
+                CloseProgram("Your software is up to date.");
+
+                stat.Text = "Status: DONE";
+                //current.Text = "Current Version: " + clientRevision.ToString();
+                current.Text = "Current Version: " + File.ReadAllText(revisionFile);
+                up.Text = "PLAY";
+            }
+            else
+            {
+                try
+                {
+
+                    Process.Start(setarry[1]);
+                    Application.Exit();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(setarry[3]);
+                    stat.Text = "Status: Error Opening Game";
+                }
+            }
+
+        }
+
+        private void progress_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void play_Click(object sender, EventArgs e)
+        {
+           
+                Process.Start(setarry[11]);
+            
+            
+        }
+
+        private static byte[] DownloadFile(string url)
+        {
+            try
+            {
+                var request = FileWebRequest.Create(url);
+                using (var response = request.GetResponse())
+                {
+                    using (var stream = response.GetResponseStream())
+                    {
+                        using (var memory = new MemoryStream())
+                        {
+                            stream.CopyTo(memory);
+                            return memory.ToArray();
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            CloseProgram("Could not download the file at " + url);
+            return null;
+        }
+
+        private static void CloseProgram(string msg)
+        {
+
+            Console.WriteLine(msg);
+            Console.WriteLine("Press any key to continue...");
+
+            //Console.Read();
+            //Console.ReadKey(false);
+
+           // Environment.Exit(1);
+        }
+
+        //private void db_connection()
+        //{
+        //    try
+        //    {
+        //        conn = "Server=so1.infinitysrv.com;Database=xbytex10_test;Uid=xbytex10_admin;Pwd=114690;";
+        //        connect = new MySqlConnection(conn);
+        //        connect.Open();
+        //    }
+        //    catch (MySqlException e)
+        //    {
+        //        throw;
+        //    }
+        //}
+
+        //private bool validate_login(string user, string pass)
+        //{
+        //    db_connection();
+        //    MySqlCommand cmd = new MySqlCommand();
+        //    string hpass;
+           
+        //    cmd.CommandText = "Select * from members where username=@user and password=@pass";
+
+        //    cmd.CommandText = "Select * from members where username=@user ";
+        //    pass = cmd.ExecuteReader().ToString();
+        //    MessageBox.Show(pass);
+        //    hpass = "";
+
+        //    cmd.Parameters.AddWithValue("@user", user);
+        //    cmd.Parameters.AddWithValue("@pass", hpass);
+        //    cmd.Connection = connect;
+        //    MySqlDataReader login = cmd.ExecuteReader();
+        //    if (login.Read())
+        //    {
+        //        connect.Close();
+        //        return true;
+        //    }
+        //    else
+        //    {
+        //        connect.Close();
+        //        return false;
+        //    }
+        //}
+
+        //private void submit_Click_1(object sender, EventArgs e)
+        //{
+
+
+
+        //    string user = username.Text;
+        //    string pass = password.Text;
+        //    if (user == "" || pass == "")
+        //    {
+        //        MessageBox.Show("Empty Fields Detected ! Please fill up all the fields");
+        //        return;
+        //    }
+        //    bool r = validate_login(user, pass);
+        //    if (r)
+        //        MessageBox.Show("Correct Login Credentials");
+        //    else
+        //        MessageBox.Show("Incorrect Login Credentials");
+        //}
+    }
+}
